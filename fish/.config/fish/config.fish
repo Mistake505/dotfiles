6ee3aa4f21db06dcc26d064efdc1a780139ddc16
Ckjos <ckjos2010@gmail.com>
2026-08-11 16:44:20 -0700

    change noctalia and fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -12,3 +12,14 @@ alias rebuild "sudo nixos-rebuild switch"
 alias rebuild-upgrade "sudo nixos-rebuild switch --upgrade"
 alias nixedit "nvim /etc/nixos/"
 alias editnix "nvim /etc/nixos/"
+
+function y
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    command yazi $argv --cwd-file="$tmp"
+    if read -z cwd <"$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
+        builtin cd -- "$cwd"
+    end
+    command rm -f -- "$tmp"
+end
+
+zoxide init fish | source
